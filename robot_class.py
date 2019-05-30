d14895c0031fb55e1c225733be72b11a701820b6
Abdelrahman44 <abdelrahman.m.mohammed@gmail.com>
2019-05-30 12:35:52 +0200

    feat: Add robot initialization

--- robot_class.py
+++ robot_class.py
@@ -1,2 +1,30 @@
-### Thi is the robot class, which contains robots attributes and methods for navigating and sensing the world
+### Thi is the robot class, which contains robots attributes and methods for navigating and sensing the world, it also generates the world landamrks
 
+class robot:
+    
+    def __init__(self, world_size=100, sensor_range=100, sensor_noise=2, actuator_noise=2):
+        #robot's sensor and actuator are assumed to have noise, which affects the robot's state and measurements
+        # the robot is also assumed to be initially at the world's center point
+        
+        self.world_size = world_size
+        self.sensor_range = sensor_range
+        self.sensor_noise = sensor_noise
+        self.actuator_noise = actuator_noise
+        self.x = self.y = self.world_size/2
+        
+    def move():
+        
+        
+        
+        
+        
+    def sense():
+        
+        
+        
+    def make_landmarks():
+        
+        
+    
+        
+        
